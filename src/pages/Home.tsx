ab8b6a0d96fe91566fdb67f68a0b9b1d6cb88807
zhangtao25 <tzhangm@trip.com>
2022-06-06 11:13:41 +0800

    添加rome

---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+import { Text, useColorScheme, View } from "react-native";
+import React from "react";
+// import {Colors} from "react-native/Libraries/NewAppScreen";
+
+const Home = () => {
+  return (<View><Text>asfas</Text></View>);
+};
+
+export default Home;
